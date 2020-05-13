caf3617f078795c4b7494e95f58f648c2f14cb37
Sebibebi67 <sebibebi67@gmail.com>
2020-05-13 11:23:11 +0200

    brain as an object

--- ai/brain.js
+++ ai/brain.js
@@ -3,45 +3,87 @@
 
 // var test = require("./test");
 var RiveScript = require('rivescript');
-var bot = undefined;
+// var bot = undefined;
 
 //===========================
 // Functions
 
-function loadingDone() {
-    console.log("Success Loading\n");
-    bot.sortReplies();
-    response("local-user", "hello");
-}
+// function loadingDone() {
+//     console.log("Success Loading\n");
+//     bot.sortReplies();
+//     response("local-user", "hello");
+// }
 
-function loadingError(error) {
-    console.log("Failed Loading: " + error);
-}
+// function loadingError(error) {
+//     console.log("Failed Loading: " + error);
+// }
 
-module.exports.loading = function(){
-    bot = new RiveScript();
+// module.exports.loading = function(){
+//     bot = new RiveScript();
 
-    //Loading a directory and a file
-    bot.loadDirectory("./ai/brain").then(loadingDone).catch(loadingError);
+//     //Loading a directory and a file
+//     bot.loadDirectory("./ai/brain").then(loadingDone).catch(loadingError);
 
-    bot.loadFile("./ai/brain/standard.rive");
-}
-
-
-/**
- * Returns an answer to the message send by a user
- * @param {String} username 
- * @param {String} message
- * @author Tony CHOUTEAU
- * @author Sébastien HERT
- */
-var response = function(username, message){
-    bot.reply(username, message).then(function (reply){
-        console.log(reply);
-    });
-}
+//     bot.loadFile("./ai/brain/standard.rive");
+// }
 
 
+// /**
+//  * Returns an answer to the message send by a user
+//  * @param {String} username 
+//  * @param {String} message
+//  * @author Tony CHOUTEAU
+//  * @author Sébastien HERT
+//  */
+// var response = function(username, message){
+//     bot.reply(username, message).then(function (reply){
+//         console.log(reply);
+//     });
+// }
 
+class Brain {
+    // bot = undefined;
+    // name = undefined;
+    // repo = undefined;
+    // files = undefined;
+    
+    constructor(name, repo, file){
+        this.bot = new RiveScript({utf8: true});
+        this.name = name;
+        this.repo = repo;
+        this.file = file;
+        
+        this.loading();
+    }
+    
+    
+    loading(){
+        
+        //Loading a directory and a file
+        this.bot.loadDirectory(this.repo).then( () => {
+            this.loadingDone();
+        }).catch(this.loadingError);
+        
+        this.bot.loadFile(this.repo+'/'+this.file);
+    }
+    
+    loadingDone(){
+        console.log("Success Loading\n");
+        this.bot.sortReplies();
+        // this.response("local-user", "hello");
+    }
+    
+    loadingError(error) {
+        console.log("Failed Loading: " + error);
+    }
+    
+    response(username, message){
+        this.bot.reply(username, message).then(function (reply){
+            console.log(reply);
+        });
+    }
 
+    
+};
 
+module.exports = Brain;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var path = require('path');
 
-var ai = require('../ai/brain');
+var Brain = require('../ai/brain');
+// import Brain from '../ai/brain';
 
 var router = express.Router();
 
 //===========================
 // Load AI
 
-ai.loading();
+var bot1 = new Brain('Ally', './ai/brain','standard.rive');
+setTimeout(()=> {bot1.response("local-user", "hello");}, 100);
 
 //===========================
 // Routes
