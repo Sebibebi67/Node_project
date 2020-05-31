e325f0b32ae69e804e322e2b5dd52cfbc37492a7
TonyChouteau <tony.chouteau@outlook.fr>
2020-05-31 02:38:58 +0200

    add request & testx.sh

---
+++ testx.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+
+
+#=================================== test.sh ====================================#
+
+
+#--------------------------------- Description ----------------------------------#
+#
+# This script tests the REST API and some commands you could use on it.
+# It tests all the command written in request.txt
+#
+#--------------------------------------------------------------------------------#
+
+
+#----------------------------------- Synopsis -----------------------------------#
+#
+# ./test.sh
+#
+#--------------------------------------------------------------------------------#
+
+
+#----------------------------------- Options ------------------------------------#
+#
+# There isn't any option for this script
+# 
+# On Windows : sed -i 's/\r$//' ./test.sh 
+# OR dos2unix ./test.sh
+#
+#--------------------------------------------------------------------------------#
+
+
+#----------------------------------- Authors ------------------------------------#
+#
+# Sébastien HERT
+#
+#--------------------------------------------------------------------------------#
+
+
+#------------------------------- Global Variables -------------------------------#
+
+file="./request.txt"
+lenTitle=80
+nbLine=1
+
+#--------------------------------------------------------------------------------#
+
+
+#---------------------------------- Functions -----------------------------------#
+
+title(){
+	###
+	# Description : Displays a Title in the terminal
+	#
+	# Input :
+	# - The separator (=,-,*, etc)
+	# - The title to display as a String
+	#
+	# Output :
+	# - Display in a shell terminal
+	#
+	# Authors :
+	# - Sébastien HERT
+	###
+
+	if [ $# -eq 0 ]; then
+        param="Titre"
+        id="="
+    elif [ $# -eq 1 ]; then
+        param=$1
+        id="="
+    elif [ $# -eq 2 ]; then
+        param=$2
+        id=$1
+    else
+        echo error
+    fi
+
+    lenParam=${#param}
+    lenEq1=$(( ($lenTitle-$lenParam)/2 -1 ))
+    lenEq2=$(( $lenTitle-$lenParam-$lenEq1-2 ))
+    title=""
+    for (( i = 0; i < $lenEq1; i++ )); do
+        title="${title}$id"
+    done
+    title="${title} $param "
+    for (( i = 0; i < $lenEq2; i++ )); do
+        title="${title}$id"
+    done
+
+    echo -e "\e[35m\e[1m${title}\n\e[0m"
+}
+
+checkEndOfFile(){
+	###
+	# Description : Checks if a file finishes with an empty line
+	#
+	# Input :
+	# - The file
+	#
+	# Output :
+	# - None, but adding a new line if its necessary
+	#
+	# Authors :
+	# - Sébastien HERT
+	###
+
+	end=$( tail -n 1 $file )
+	if [[ $end != "" ]]; then
+		echo "" >> $file
+	fi
+}
+
+erase(){
+	directories="./data/mouths"
+	for d in $directories; do
+		if [[ -d $d ]]; then
+			rm -r $d
+		fi
+		if [[ ! -d $d ]]; then
+			mkdir $d
+		fi
+	done
+}
+
+
+#--------------------------------------------------------------------------------#
+
+
+#------------------------------------- Main -------------------------------------#
+
+set -e
+
+checkEndOfFile
+
+erase
+
+title "=" "Testing REST API"
+
+while read line
+do
+	if [[ "${line::1}" == [a-z] ]]; then
+	
+		command=$(echo $line | cut -d';' -f1 )
+
+		expected=$(echo $(echo $line | cut -d';' -f2 ))
+
+		statusExpected=$(echo $(echo $line | cut -d';' -f3 | sed 's/[^0-9]//g'))
+
+		fullOutput=${command/-X/'--write-out %{http_code} --silent -X'}
+		
+		output=$($fullOutput)
+
+
+		status=${output: -3}
+		output=${output:0: -3}
+
+		# title "-" "Testing Command Line $nbLine"
+
+		# echo -e "\e[36mCommand Tested :\e[0m $command\n"
+
+		# echo -e "\e[36mOutput expected :\e[0m Status : $statusExpected ; $expected\n"
+
+		if [[ $status == $statusExpected ]]; then
+			printf "\e[36m Command Tested : \e[0m%-130s | \e[36mExpected Output : \e[0m%s \e[36mOutput : \e[0m%s | \e[36mStatus : \e[32mOK\n" "$command" "$statusExpected" "$status"
+			# echo -e -n "\e[36mOutput : \e[0m\e[32mStatus : $status ; $output\e[0m\n\n"
+		else
+			printf "\e[36m Command Tested : \e[0m%-130s | \e[36mExpected Output : \e[0m%s \e[36mOutput : \e[0m%s | \e[36mStatus : \e[31mKO\n" "$command" "$statusExpected" "$status"
+			#echo -e -n "\e[36mOutput : \e[0m\e[31mStatus : $status ; $output\e[0m\n\n"
+		fi
+
+	fi
+	(( nbLine++))
+done < $file
+
+title End
+
+#--------------------------------------------------------------------------------#
+
+
+#================================================================================#
+
+
+
+
+
+
+
