6eeedcdc42189d7c80b56dd814c34e3d2fb0b1ba
TonyChouteau <tony.chouteau@outlook.fr>
2020-06-03 17:09:25 +0200

    discord set age to user.id

--- bot/bot-manager.js
+++ bot/bot-manager.js
@@ -131,7 +131,7 @@ class BotsManager {
 						}
 						if (msg.content.startsWith(name)){
 							let message = msg.content.split(name+" ").join("");
-							brain.response("NaN", message).then((reply) => {
+							brain.response(message.member.id, message).then((reply) => {
 								msg.reply(reply.split("$name").join("**"+name+"**"));
 							});
 							
